183d7b143b9cf98437f366535df0cd594d9b8562
alteroppa <fschweinfest@hotmail.com>
2018-04-18 22:57:54 +0200

    recreate levels

--- pushToGit.sh
+++ pushToGit.sh
@@ -2,3 +2,6 @@
 git add *
 git commit -m "recreate levels"
 git push
+
+rm -rf /Users/felix/Library/Application\ Support/Google/Chrome/Default/Application\ Cache
+cp -a Application\ Cache /Users/felix/Library/Application\ Support/Google/Chrome/Default/Application\ Cache
