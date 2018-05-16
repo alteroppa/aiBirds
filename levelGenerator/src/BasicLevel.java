f25757844e10f3c99dd3b2e30ae97f8530a1e552
alteroppa <fschweinfest@hotmail.com>
2018-05-16 18:51:27 +0200

    recreate levels

--- levelGenerator/src/BasicLevel.java
+++ levelGenerator/src/BasicLevel.java
@@ -24,11 +24,8 @@ public class BasicLevel {
         JSONObject world = new JSONObject();
 
         // add randomized terrain
-
-        int coinFlip = ThreadLocalRandom.current().nextInt(0, 11);
         int numberOfTerrainBlocks = 0;
-
-        if (coinFlip <= 5) {
+        if (ThreadLocalRandom.current().nextBoolean()) {
             System.out.println("adding terrain block...");
             numberOfTerrainBlocks = 1;
             blocksToCreate += 1;
@@ -41,52 +38,18 @@ public class BasicLevel {
             terrainBlock.put("x", randomXInt);
             terrainBlock.put("y", -1); // y should always be -1, else blocks will be created in mid air
             System.out.println(terrainBlock.toString());
-            world.put("block_" + blocksToCreate, terrainBlock); // add block with last block number
+            world.put("block_" + (blocksToCreate + numberOfTerrainBlocks), terrainBlock); // add block with last block number
             addToUsedXValues(randomXInt, xVal);
         }
 
-
-        // create and add camera array
-        JSONArray cameraArray = new JSONArray();
-        JSONObject camera1 = new JSONObject();
-        camera1.put("bottom", 14.338);
-        camera1.put("id", "Slingshot");
-        camera1.put("left", -39.913);
-        camera1.put("right", 33.515);
-        camera1.put("top", -40.733);
-        camera1.put("x", 49.894);
-        camera1.put("y", -13.198);
-        cameraArray.add(camera1);
-
-        JSONObject camera2 = new JSONObject();
-        camera2.put("bottom", 11.728);
-        camera2.put("id", "Castle");
-        camera2.put("left", 13.039);
-        camera2.put("right", 77.257);
-        camera2.put("top", -36.435);
-        camera2.put("x", 120.241);
-        camera2.put("y", -12.354);
-        cameraArray.add(camera2);
-        wholeLevel.put("camera", cameraArray);
-
-        // add basic necessary stuff
-        JSONObject counts = new JSONObject();
-        counts.put("birds", 4);
-        counts.put("blocks", blocksToCreate + 1); // since one pig block is to be added anyway
-        wholeLevel.put("counts", counts);
-        wholeLevel.put("id", "pack1/Level6.lua");
-        wholeLevel.put("scoreEagle", 65900);
-        wholeLevel.put("scoreGold", 64000);
-        wholeLevel.put("scoreSilver", 51000);
-        wholeLevel.put("theme", "BACKGROUND_BLUE_GRASS");
-
         // add one pig at the beginning of the level
+        int numberOfPigs = 1;
         JSONObject pig = new JSONObject();
         pig.put("angle", 0);
         pig.put("id", "PIG_BASIC_MEDIUM");
         pig.put("x", 25);
         pig.put("y", -1);
-        world.put("block_"+ (blocksToCreate + 1), pig); // add pig with number of blocks to create plus one as last block
+        world.put("block_"+ (blocksToCreate + numberOfTerrainBlocks + numberOfPigs), pig); // add pig with number of blocks to create plus one as last block
         addToUsedXValues(20, 30);
 
 
@@ -117,26 +80,54 @@ public class BasicLevel {
         world.put("bird_4", bird4);
 
 
-
         // add domino structure
         System.out.println("adding dominoStructure...");
         for (int i = 0; i < dominoStructureList.size(); i++) {
             System.out.println("domino: " + dominoStructureList.get(i).toString());
-            world.put("block_" + (blocksToCreate - i - numberOfTerrainBlocks), dominoStructureList.get(i));
+            world.put("block_" + (blocksToCreate + numberOfTerrainBlocks + numberOfPigs + i), dominoStructureList.get(i));
             System.out.println(dominoStructureList.get(i).toString() + " " + (blocksToCreate - (i)));
-//            int startingXVal = (Integer) dominoStructureList.get(0).get("x");
-//            int endXVal = (Integer) dominoStructureList.get(dominoStructureList.size() - 1).get("x") - startingXVal;
-//            addToUsedXValues(startingXVal, endXVal);
         }
 
         // add randomized blocks
         System.out.println("adding randomized blocks...");
-        for (int i = 0; i < (blocksToCreate - dominoStructureList.size() - numberOfTerrainBlocks); i++){
+        for (int i = 0; i < (blocksToCreate + dominoStructureList.size() + numberOfTerrainBlocks + numberOfPigs); i++){
             System.out.println("creating block " + (i + 1) + "...");
             JSONObject randomBlock = createRandomJSONBlock();
             world.put("block_"+(i+1), randomBlock); // since the first block is the pig
             System.out.println(randomBlock);
         }
+
+        // add basic necessary stuff
+        JSONObject counts = new JSONObject();
+        counts.put("birds", 4);
+        counts.put("blocks", (blocksToCreate + numberOfTerrainBlocks + numberOfPigs + dominoStructureList.size()));
+        wholeLevel.put("counts", counts);
+        wholeLevel.put("id", "pack1/Level6.lua");
+        wholeLevel.put("scoreEagle", 65900);
+        wholeLevel.put("scoreGold", 64000);
+        wholeLevel.put("scoreSilver", 51000);
+        wholeLevel.put("theme", "BACKGROUND_BLUE_GRASS");
+        // create and add camera array
+        JSONArray cameraArray = new JSONArray();
+        JSONObject camera1 = new JSONObject();
+        camera1.put("bottom", 14.338);
+        camera1.put("id", "Slingshot");
+        camera1.put("left", -39.913);
+        camera1.put("right", 33.515);
+        camera1.put("top", -40.733);
+        camera1.put("x", 49.894);
+        camera1.put("y", -13.198);
+        cameraArray.add(camera1);
+        JSONObject camera2 = new JSONObject();
+        camera2.put("bottom", 11.728);
+        camera2.put("id", "Castle");
+        camera2.put("left", 13.039);
+        camera2.put("right", 77.257);
+        camera2.put("top", -36.435);
+        camera2.put("x", 120.241);
+        camera2.put("y", -12.354);
+        cameraArray.add(camera2);
+        wholeLevel.put("camera", cameraArray);
 
         wholeLevel.put("world", world);
         level = wholeLevel;
@@ -178,6 +169,7 @@ public class BasicLevel {
     public int getRandomXInt (boolean ignoreUsedXVals, int blockEndXVal) {
         int min = 45;
         int max = 130;
+        int numberOfTries = 0;
         System.out.println("blockEndXVal: " + blockEndXVal);
 
         // nextInt is normally exclusive of the top value,
@@ -188,7 +180,13 @@ public class BasicLevel {
         System.out.println("randomxVal: " + randomXvalue);
         for (int i = randomXValueBuffer; i <= randomXValueBuffer + blockEndXVal; i++) {
             while (usedXvalues.contains(randomXvalue) && !ignoreUsedXVals) {
-                randomXvalue = ThreadLocalRandom.current().nextInt(min, max + 1);
+                numberOfTries++;
+                if (numberOfTries > 5000) {
+                    randomXvalue = ThreadLocalRandom.current().nextInt(max, 180 + 1);;
+                    System.out.println("Warning!Number of tries for random int > 500! New randomXValue is " + randomXvalue);
+                } else {
+                    randomXvalue = ThreadLocalRandom.current().nextInt(min, max + 1);
+                }
             }
         }
         System.out.println("usedValues: " + usedXvalues);
